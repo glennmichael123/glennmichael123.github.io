d943d1d7896a9d202e048fac0142c33d646d9bd8
Glenn Michael Torregosa <gtorregosa@gmail.com>
2022-05-25 18:39:35 +0800

    first commit

---
+++ precache-manifest.a56500a64de422ebd6fac7bfb369b137.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef2f83d5a22a4328e398",
+    "url": "/css/app.f9398c35.css"
+  },
+  {
+    "revision": "26c768a6bd479b353f9b",
+    "url": "/css/chunk-vendors.f9c547d1.css"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/fonts/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/fonts/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/fonts/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/fonts/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/img/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "fcf6c093f6085cc0262571a5b17b7156",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ef2f83d5a22a4328e398",
+    "url": "/js/app.fc0c8f16.js"
+  },
+  {
+    "revision": "8e621b4b2ac060ce0b6e",
+    "url": "/js/chunk-1622d6e8.723fe3af.js"
+  },
+  {
+    "revision": "e5a12d6e61c46d4f0936",
+    "url": "/js/chunk-2d216214.1ba829b5.js"
+  },
+  {
+    "revision": "2a3e2d8ab37c41fb33f7",
+    "url": "/js/chunk-2d216257.1caeb235.js"
+  },
+  {
+    "revision": "c70a3b56bcebe46e36e1",
+    "url": "/js/chunk-3660fae1.27a45571.js"
+  },
+  {
+    "revision": "26c768a6bd479b353f9b",
+    "url": "/js/chunk-vendors.63eefd61.js"
+  },
+  {
+    "revision": "a9bb04712052819ce9a0c309d4d18a1e",
+    "url": "/manifest.json"
+  }
+]);
